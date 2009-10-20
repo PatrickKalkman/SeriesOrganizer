57b597a6104b70bb5e69f6c50f820c00b3266818
kalkie <pkalkie@gmail.com>
2009-10-20 07:17:54 +0200

    added files

---
+++ SubtitlesManagement/CustomTextMessageEncoderFactory.cs
@@ -0,0 +1,52 @@
+﻿using System.ServiceModel.Channels;
+
+namespace Chalk.SubtitlesManagement
+{
+   public class CustomTextMessageEncoderFactory : MessageEncoderFactory
+   {
+      private readonly MessageEncoder encoder;
+      private readonly MessageVersion version;
+      private readonly string mediaType;
+      private readonly string charSet;
+
+      internal CustomTextMessageEncoderFactory(string mediaType, string charSet, MessageVersion version)
+      {
+         this.version = version;
+         this.mediaType = mediaType;
+         this.charSet = charSet;
+         this.encoder = new CustomTextMessageEncoder(this);
+      }
+
+      public override MessageEncoder Encoder
+      {
+         get
+         {
+            return this.encoder;
+         }
+      }
+
+      public override MessageVersion MessageVersion
+      {
+         get
+         {
+            return this.version;
+         }
+      }
+
+      internal string MediaType
+      {
+         get
+         {
+            return this.mediaType;
+         }
+      }
+
+      internal string CharSet
+      {
+         get
+         {
+            return this.charSet;
+         }
+      }
+   }
+}
---
+++ SubtitlesManagement/FilterMessageInspector.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.ServiceModel;
+using System.ServiceModel.Dispatcher;
+using System.ServiceModel.Channels;
+using System.Globalization;
+using System.Text;
+using System.Threading;
+using System.Xml;
+
+namespace Chalk.SubtitlesManagement
+{
+   public class FilterMessageInspector : IClientMessageInspector, IDispatchMessageInspector
+   {
+      private const string CultureHeaderKey = "culture";
+
+      #region IDispatchMessageInspector Members
+
+      public object AfterReceiveRequest(ref Message request, IClientChannel channel, InstanceContext instanceContext)
+      {
+         int headerIndex = request.Headers.FindHeader(CultureHeaderKey, string.Empty);
+         if (headerIndex != -1)
+         {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo(request.Headers.GetHeader<string>(headerIndex));
+         }
+
+         return null;
+      }
+
+      public void BeforeSendReply(ref Message reply, object correlationState)
+      {
+
+      }
+
+      #endregion
+
+      #region IClientMessageInspector Members
+
+      public void AfterReceiveReply(ref Message reply, object correlationState)
+      {
+         MessageBuffer buffer = reply.CreateBufferedCopy(int.MaxValue);
+         
+         MemoryStream stream = new MemoryStream();
+         buffer.WriteMessage(stream);
+         string payLoad = GetStringFromMemoryStream(stream);
+
+         int i = 10;
+      }
+
+      public static string GetStringFromMemoryStream(MemoryStream m)
+      {
+         if (m == null || m.Length == 0)
+            return null;
+
+         m.Flush();
+         m.Position = 0;
+         StreamReader sr = new StreamReader(m);
+         string s = sr.ReadToEnd();
+
+         return s;
+      }
+
+
+      public object BeforeSendRequest(ref Message request, IClientChannel channel)
+      {
+         return null;
+      }
+
+      #endregion
+   }
+}
