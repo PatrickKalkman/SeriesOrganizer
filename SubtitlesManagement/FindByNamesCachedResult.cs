6697afc3a709282ce2b8128d5365efb3d789ed50
kalkie <pkalkie@gmail.com>
2009-10-21 12:58:10 +0200

    subtitles

---
+++ SubtitlesManagement/FindByNamesCachedResult.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace Chalk.SubtitlesManagement
+{
+   [XmlRoot(ElementName = "bierdopje", Namespace = "", IsNullable = false)]
+   [Serializable()]
+   public class FindByNamesCachedResult : ITvShowResult 
+   {
+      [XmlArray("response")]
+      [XmlArrayItem("result", typeof(TvShow))]
+      public List<TvShow> tvShows;
+
+      public List<TvShow> TvShows
+      {
+         get { return tvShows; }
+      }
+   }
+}
