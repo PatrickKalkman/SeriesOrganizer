25b454e2f4c2f126e986822d19b73d5cece21a99
kalkie <pkalkie@gmail.com>
2009-11-03 14:11:39 +0100

    Added new files.

---
+++ SubtitlesManagement/ServiceChannelFactory.cs
@@ -0,0 +1,23 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.ServiceModel.Web;
+
+namespace Chalk.SubtitlesManagement
+{
+   public class ServiceChannelFactory
+   {
+      private readonly WebChannelFactory<ITvSeries> channelFactory;
+
+      public ServiceChannelFactory(SubtitlesConfigurationType configuration)
+      {
+         string uri = string.Format("{0}/api/{1}", configuration.BierdopjeUrl, configuration.BierdopjeApiKey);
+         ServiceEndpoint endPoint = new ServiceEndpoint(ContractDescription.GetContract(typeof(ITvSeries)), new WebHttpBinding(), new EndpointAddress(uri));
+         channelFactory = new WebChannelFactory<ITvSeries>(endPoint);
+      }
+
+      internal virtual ITvSeries CreateChannel()
+      {
+         return channelFactory.CreateChannel();
+      }
+   }
+}
---
+++ SubtitlesManagementTest/SubtitlesServiceTest.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Chalk.SubtitlesManagement.Models;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace Chalk.SubtitlesManagement
+{
+   [TestFixture]
+   public class SubtitlesServiceTest
+   {
+      private readonly MockRepository repository = new MockRepository();
+
+      [Test]
+      public void ShouldSendResponseStringToResponseParser()
+      {
+         SubtitlesService subtitlesService = CreateSubtitleServiceAndSetExpectations();
+         repository.ReplayAll();
+         subtitlesService.FindShowsByName("Flash");
+         repository.VerifyAll();
+      }
+
+      private SubtitlesService CreateSubtitleServiceAndSetExpectations()
+      {
+         const string ResponseString = "Dummy Response String";
+
+         SubtitlesServiceResponseParser responseParser = repository.DynamicMock<SubtitlesServiceResponseParser>();
+         Expect.Call(responseParser.GetTvShows(ResponseString)).Return(new List<TvShow>());
+
+         ITvSeries tvSeries = repository.DynamicMock<ITvSeries>();
+         Expect.Call(tvSeries.FindShowByName(string.Empty)).Return(CreateDummyStream(ResponseString)).IgnoreArguments();
+
+         ServiceChannelFactory serviceChannelFactory = repository.DynamicMock<ServiceChannelFactory>(CreateSubtitlesConfiguration());
+         Expect.Call(serviceChannelFactory.CreateChannel()).Return(tvSeries);
+
+         return new SubtitlesService(responseParser, serviceChannelFactory);
+      }
+
+      private static Stream CreateDummyStream(string responseString)
+      {
+         return new MemoryStream(Encoding.Default.GetBytes("bladiebla"));
+      }
+
+      private static SubtitlesConfigurationType CreateSubtitlesConfiguration()
+      {
+         SubtitlesConfigurationType configuration = new SubtitlesConfigurationType();
+         configuration.BierdopjeUrl = "http://www.bierdopje.com";
+         configuration.BierdopjeApiKey = "12121212";
+         return configuration;
+      }
+   }
+}
