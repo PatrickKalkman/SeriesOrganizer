5c0a051bfbdd32cf2147e8c4487dc8a0abe35ec7
kalkie <pkalkie@gmail.com>
2009-12-17 16:08:55 +0100

    merging

--- SeriesManagement/SerieMover.cs
+++ SeriesManagement/SerieMover.cs
@@ -15,13 +15,24 @@ namespace Chalk.SerieOrganizer
       {
          if (serie.IsValid)
          {
-            string destinationDirectory = Path.Combine(destination, serie.Name);
-            if (!Directory.Exists(destinationDirectory))
-            {
-               Directory.CreateDirectory(destinationDirectory);
-            }
-            File.Move(serie.FullName, Path.Combine(destinationDirectory, serie.FileName));
+            string destinationDirectory = CreateDestinationDirectory(serie);
+
+            string destinationFileName = Path.Combine(destinationDirectory, serie.FileName);
+            if (File.Exists(destinationFileName))
+               File.Delete(destinationFileName);
+
+            File.Move(serie.FullName, destinationFileName);
          }
+      }
+
+      private string CreateDestinationDirectory(Serie serie)
+      {
+         string destinationDirectory = Path.Combine(destination, serie.Name);
+         if (!Directory.Exists(destinationDirectory))
+         {
+            Directory.CreateDirectory(destinationDirectory);
+         }
+         return destinationDirectory;
       }
    }
 }
--- SubtitlesManagement/ITvSeries.cs
+++ SubtitlesManagement/ITvSeries.cs
@@ -6,28 +6,20 @@ namespace Chalk.SubtitlesManagement
    /// </summary>
    internal interface ITvSeries
    {
-//      [WebInvoke(UriTemplate = "/GetShowById/{showId}", Method = "Get")]
       string GetShowById(string showId);
 
-//      [WebInvoke(UriTemplate = "/GetShowByTVDBID/{showId}", Method = "Get")]
       string GetShowByTvDbId(string showId);
 
-//      [WebInvoke(UriTemplate = "/FindShowByName/{name}", Method = "Get")]
       string FindShowByName(string name);
 
-//      [WebInvoke(UriTemplate = "/GetEpisodesForSeason/{showId}/{season}", Method = "Get")]
       string GetEpisodesForSeason(string showId, string season);
 
-//      [WebInvoke(UriTemplate = "/GetAllEpisodesForShow/{showId}", Method = "Get")]
       string GetAllEpisodesForShow(string showId);
 
-//      [WebInvoke(UriTemplate = "/GetEpisodeById/{episodeId}", Method = "Get")]
       string GetEpisodeById(string episodeId);
 
-//      [WebInvoke(UriTemplate = "/GetAllSubsForEpisode/{episodeId}/{language}", Method = "Get")]
       string GetAllSubsForEpisode(string episodeId, string language);
 
-//      [WebInvoke(UriTemplate = "/GetAllSubsFor/{showId}/{season}/{episodeId}/{language}/{isTvDbId}", Method = "Get")]
       string GetAllSubsFor(string showId, string season, string episodeId, string language, string isTvDbId);
    }
 }
