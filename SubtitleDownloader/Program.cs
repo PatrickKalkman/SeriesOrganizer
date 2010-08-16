cb42cc9940418e90367e35adb84573593d147d7d
kalkie <pkalkie@gmail.com>
2010-08-16 12:03:20 +0200

    Added subdirectory enumeration and subtitle download

--- SubtitleDownloader/Program.cs
+++ SubtitleDownloader/Program.cs
@@ -1,22 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
 using Chalk.SerieOrganizer;
 using Chalk.SubtitlesManagement;
 
-namespace SubtitleDownloaderApplication
+namespace Chalk.SubtitleDownload
 {
    class Program
    {
       static void Main(string[] args)
       {
-         SerieCollector serieCollector = new SerieCollector(args[0]);
+         DirectoryInfo directoryInfo = new DirectoryInfo(args[0]);
+         DirectoryInfo[] directories = directoryInfo.GetDirectories();
+         DownloadSubtitlesForDirectory(directoryInfo);
+         foreach (DirectoryInfo directory in directories)
+         {
+            DownloadSubtitlesForDirectory(directory);
+         }
+      }
+
+      private static void DownloadSubtitlesForDirectory(DirectoryInfo directory)
+      {
+         SerieCollector serieCollector = new SerieCollector(directory.FullName);
          List<Serie> series = serieCollector.Collect();
 
          SubtitleService subTitleService = SubtitleServiceFactory.CreateSubtitleService();
-	 SubtitleDownloader subTitleDownloader = new SubtitleDownloader(subTitleService);
+         SubtitleDownloader subTitleDownloader = new SubtitleDownloader(subTitleService);
          foreach (Serie serie in series)
          {
             string subtitleDirectory = new FileInfo(serie.FullName).DirectoryName;
