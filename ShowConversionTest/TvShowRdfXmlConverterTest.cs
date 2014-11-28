9145e05ac6a55090b712beaf0a329755487ec3b2
Patrick Kalkman <pkalkie@gmail.com>
2014-11-28 10:49:38 +0100

    Make string comparison ignore difference in platform

--- ShowConversionTest/TvShowRdfXmlConverterTest.cs
+++ ShowConversionTest/TvShowRdfXmlConverterTest.cs
@@ -1,23 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using Chalk.SubtitlesManagement.Models;
+﻿using Chalk.SubtitlesManagement.Models;
 using NUnit.Framework;
 using ShowConversion;
 
 namespace ShowConversionTest
 {
-   [TestFixture]
-   public class TvShowRdfXmlConverterTest
-   {
-      [Test]
-      public void ShouldGenerateCorrectRdfXmlFromTvShow()
-      {
-         TvShow show = TvShowTestUtilities.CreateNewTvShow();
-         TvShowRdfXmlConverter tvShowRdfConverter = new TvShowRdfXmlConverter();
-         string tvShowRdfXml = tvShowRdfConverter.Convert(show);
-         Assert.AreEqual(ShowConversionTest.ConvertedRDFFromShow, tvShowRdfXml);
-      }
-
-
-   }
+    [TestFixture]
+    public class TvShowRdfXmlConverterTest
+    {
+        [Test]
+        public void ShouldGenerateCorrectRdfXmlFromTvShow()
+        {
+            TvShow show = TvShowTestUtilities.CreateNewTvShow();
+            var tvShowRdfConverter = new TvShowRdfXmlConverter();
+            string tvShowRdfXml = tvShowRdfConverter.Convert(show);
+            string expectedResult = ShowConversionTest.ConvertedRDFFromShow.Replace("\r\n", "\n");
+            string result = tvShowRdfXml.Replace("\r\n", "\n");
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
 }
